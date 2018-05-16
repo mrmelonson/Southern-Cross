dc91e98b74c3178369296b5fd94ee937b2ac607b
Zelenyy <dangerdude119@gmail.com>
2018-05-16 22:08:45 +1000

    server link chznagecdeds
    > Changed server link as it was expired

--- LunaBot/ServerUtilities/KickUserHelper.cs
+++ LunaBot/ServerUtilities/KickUserHelper.cs
@@ -27,7 +27,7 @@ namespace LunaBot.ServerUtilities
                 await user.SendMessageAsync("You have been kicked from the server from inactivity.\n" +
                     "You can join again but once you get kicked 3 times you will be banned.\n" +
                     "*Hint: respond to pings and we will let you in*\n" +
-                    "https://discord.gg/tY8xfJ");
+                    "https://discord.gg/FAYVfnT");
             }
 #pragma warning disable CS0168 // Variable is declared but never used
             catch (HttpException e)
