9669837fdb391c4ea3342c0a6fc0011c1fa05e07
Zelenyy <dangerdude119@gmail.com>
2018-07-04 10:02:38 +1000

    End my eternal suffering
    > Added "Dab" command
    > Loss of all hope has been achieved

---
+++ LunaBot/Commands/User/DabCommand.cs
@@ -0,0 +1,21 @@
+﻿using System.Threading.Tasks;
+using Discord.WebSocket;
+using System.Threading;
+using System.Collections.Generic;
+using System;
+
+namespace LunaBot.Commands
+{
+    [LunaBotCommand("Dab")]
+    class DabCommand : BaseCommand
+    {
+        public override async Task ProcessAsync(SocketMessage message, string[] parameters)
+        {
+            // What the fuck am i doing with my life
+            ulong userid = message.Author.Id;
+            string[] dabflavours = {$"<@{userid}> dabs on those ni:b::b:as", $"Wow that was one hard dab <@{userid}>!", $"<@{userid}> dabs so hard their arms almost dislocate.", $"*Sigh* fine, <@{userid}> dabs. Yay! wtf is wrong with you people."}; 
+            Random r = new Random();
+            await message.Channel.SendMessageAsync(dabflavours[r.Next(dabflavours.Length)]);
+        }
+    }
+}
